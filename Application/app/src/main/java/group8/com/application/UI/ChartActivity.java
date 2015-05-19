f61da7fc410eb1833974754e6af54696deef0a11
Nicholas <niklasoti@gmail.com>
2015-05-19 16:55:10 +0200

    Update to chart
    Added a limit line to display average score(50 points) on the chart
    Enabled touch gestures: Scaling, draging and zooming
    Chart now displayed 5 min longer.

--- Application/app/src/main/java/group8/com/application/UI/ChartActivity.java
+++ Application/app/src/main/java/group8/com/application/UI/ChartActivity.java
@@ -8,6 +8,8 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -20,13 +22,8 @@ import group8.com.application.UI.mainView.menuView;
 
 public class ChartActivity extends Activity {
 
-
-
     Context context = menuView.getContext();
     CountDownTimer cdt;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +33,17 @@ public class ChartActivity extends Activity {
         setContentView(R.layout.activity_chart);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title) ;
 
-
-
         // Used to update the Arraylist
         int speed = Session.getSpeedScore();
         int distraction = Session.getDriverDistractionLevelScore();
         int brake = Session.getBrakeScore();
         int fuel = Session.getFuelConsumptionScore();
-        
-
 
         ArrayList<BarEntry> entries = new ArrayList<>();
         entries.add(new BarEntry(speed, 0));
         entries.add(new BarEntry(distraction, 1));
         entries.add(new BarEntry(brake, 2));
         entries.add(new BarEntry(fuel, 3));
-
 
         BarDataSet dataset = new BarDataSet(entries, "Points Earned");
 
@@ -61,23 +53,44 @@ public class ChartActivity extends Activity {
         labels.add("Brakes ");
         labels.add("Fuel");
 
-
         BarChart chart = new BarChart(context);
+
         setContentView(chart);
+
         BarData data = new BarData(labels, dataset);
+
         chart.setData(data);
 
-
+        // set description
         chart.setDescription("Your points");
 
-        dataset.setColors(ColorTemplate.JOYFUL_COLORS); // add colors
+        // add colors
+        dataset.setColors(ColorTemplate.JOYFUL_COLORS);
 
-        chart.animateY(5000); // Add some animations
+        // Add some animations
+        chart.animateY(5000);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setPinchZoom(true);
+
+        // add a limit line
+        LimitLine line = new LimitLine(50f, "Average");
+        line.setLineWidth(4f);
+        line.enableDashedLine(10f, 10f, 0f);
+        line.setLabelPosition(LimitLine.LimitLabelPosition.POS_RIGHT);
+        line.setTextSize(10f);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.addLimitLine(line);
 
 
-
-
-        cdt = new CountDownTimer(15000, 1000) {  //10 seconds for demonstration purposes
+        // Graph displayed for 20 seconds
+        cdt = new CountDownTimer(20000, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -86,11 +99,8 @@ public class ChartActivity extends Activity {
 
             @Override
             public void onFinish() {
-
-                //Intent nextScreen = new Intent(getApplicationContext(), mainView.class);
-
+                
                 finish ();
-
 
             }
         };
