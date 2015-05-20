eeafab66d2f8775acd55be0b43b32b48675484b2
BGextreme <krisikn95@gmail.com>
2015-05-20 15:28:04 +0200

    Style updates, dialog updates .

--- Application/app/src/main/java/group8/com/application/UI/ChartActivity.java
+++ Application/app/src/main/java/group8/com/application/UI/ChartActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Window;
+
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
